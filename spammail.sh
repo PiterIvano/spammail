f16973f1b89f49243c52fb9a17ac7c32b7fb59ee
Hidden-parker <68443246+Hidden-parker@users.noreply.github.com>
2021-09-12 22:00:36 -0600

    Create spammail.sh

---
+++ spammail.sh
@@ -0,0 +1,25 @@
+
+echo "Este Script Es Creado Por Piter"
+echo "Escriba El Correo Victima"
+
+read too
+echo "Escriba El Asunto"
+read asu
+echo "Escriba El Mensaje"
+read msg
+
+echo "Cantidad De Veces Que Quieres Enviar"
+read cont
+
+curl -i "https://pitersk.com/piter/mail.php?correo=$too&asunto=$asu&mensaje=$msg"
+
+
+x=1
+while [ $x -le $cont ]
+do
+curl -i "https://pitersk.com/piter/mail.php?correo=$too&asunto=$asu&mensaje=$msg"
+
+  echo "Correo Numero $x Enviado"
+  x=$(( $x + 1 ))
+  
+done
